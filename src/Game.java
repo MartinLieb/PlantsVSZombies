6f765558831ff82bf647632dce3a3af88cacf4b2
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-10-30 12:28:12 +0100

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Game {
+    // Initialize modstandere
+    Plant plante = new Plant();
+    Zombie zombie = new Zombie();
+
+
+    public void start(){
+        Random random = new Random();
+
+        boolean zombieHit = random.nextBoolean(); // Random boolean
+
+        while(zombie.getHealth() != 0 || plante.getHealth() != 0){
+            if (zombieHit){
+                zombie.attack(plante);
+            }
+            else{
+                plante.attack(zombie);
+            }
+            zombieHit = !zombieHit;
+        }
+
+    }
+}
